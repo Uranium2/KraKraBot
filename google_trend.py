f6a8e9c6f516e051ed6a1cdebe4a31c6a84cc36a
Antoine TAVERNIER - ThinkPad <tavernier.antoine@hotmail.fr>
2019-06-05 16:44:07 +0200

    Time + Trends + Order Book => CSV

--- google_trend.py
+++ google_trend.py
@@ -11,7 +11,9 @@ import json
 from collections.abc import Iterable
 
 from datetime import datetime, timedelta
+import time
 
+timeLoop = 300 # 300 sec = 5 min
 
 def flatten(items):
     """Yield items from any nested iterable; see Reference."""
@@ -22,58 +24,84 @@ def flatten(items):
         else:
             yield x
 
-
-pytrend = TrendReq()
-
-bad =['bitcoin', 'crash', 'ban', 'loss']
-
-good =['bitcoin', 'rise', 'skyrocket', 'gain', 'bubble']
-
 now = datetime.now()
-currentDay = now.day
-currentMonth = now.month
+currentDay = '%02d' % now.day
+currentMonth = '%02d' % now.month
 currentYear = now.year
-currentHour = now.hour
+currentHour = '%02d' % now.hour
+currentMinute = '%02d' % now.minute
+csv_file_name = "data/indicator_" + str(currentYear) + "_" + str(currentMonth) + "_" + str(currentDay) + "_" + str(currentHour) + "_" + str(currentMinute) + ".csv"
 
-before = now - timedelta(hours=2)
-beforeDay = before.day
-beforeMonth = before.month
-beforeYear = before.year
-beforeHour = before.hour
-
-
-
-
-# print("BAD GOOGLE TREND:")
-historical_interest_bad = pytrend.get_historical_interest(bad, year_start=beforeYear, month_start=beforeMonth, day_start=beforeDay, hour_start=beforeHour, year_end=currentYear, month_end=currentMonth, day_end=currentDay, hour_end=currentHour, cat=0, geo='', gprop='', sleep=0)
-print(historical_interest_bad)
-historical_interest_bad = historical_interest_bad.drop(labels='isPartial', axis=1) 
-shortBad = historical_interest_bad.sum(axis = 0, skipna = True)
-# print(shortBad)
-historical_interest_bad.to_csv("historical_interest_bad.csv", sep=',')
-dfBad = pd.read_csv('historical_interest_bad.csv', skiprows=1, index_col='date', names=['date', 'bitcoin', 'crash', 'ban', 'loss'])
-dfBad.plot()
-plt.show()
-
-# print("GOOD GOOGLE TREND:")
-# historical_interest_good = pytrend.get_historical_interest(good, year_start=beforeYear, month_start=beforeMonth, day_start=beforeDay, hour_start=beforeHour, year_end=currentYear, month_end=currentMonth, day_end=currentDay, hour_end=currentHour, cat=0, geo='', gprop='', sleep=0)
-# historical_interest_good = historical_interest_good.drop(labels='isPartial', axis=1) 
-# shortGood = historical_interest_good.sum(axis = 0, skipna = True)
-# #print(shortGood)
-# historical_interest_good.to_csv("historical_interest_good.csv", sep=',')
-# dfGood = pd.read_csv('historical_interest_good.csv', skiprows=1, index_col='date', names=['date', 'bitcoin', 'rise', 'skyrocket', 'gain', 'bubble'])
-# dfGood.plot()
-# plt.show()
+indicator = open(csv_file_name,"w")
+indicator.write("date,bitcoin,crash,ban,loss,rise,skyrocket,gain,bubble,")
+for i in range(50):
+    indicator.write("BidPrice" + str(i) + ", BidSize" + str(i) + ",")
+for i in range(50):
+    indicator.write("AskPrice" + str(i) + ", AskSize" + str(i) + ",")
+indicator.write("\r\n")
+indicator.close()
 
 
-public_client = cbpro.PublicClient()
+while True:
+    # Get time for Google Trends search 2 hour span
+    now = datetime.now()
+    currentDay = now.day
+    currentMonth = now.month
+    currentYear = now.year
+    currentHour = now.hour
+    before = now - timedelta(hours=2)
+    beforeDay = before.day
+    beforeMonth = before.month
+    beforeYear = before.year
+    beforeHour = before.hour
 
-data = public_client.get_product_order_book('BTC-USD', level=2)
-with open('data.txt', 'w') as outfile:  
-    json.dump(data, outfile, indent=4)
+    # Open Public connection CoinBase Pro 
+    public_client = cbpro.PublicClient()
+
+    # Init Trend settings
+    pytrend = TrendReq()
+    bad =['bitcoin', 'crash', 'ban', 'loss']
+    good =['bitcoin', 'rise', 'skyrocket', 'gain', 'bubble']
+
+    
+        # BAD TREND
+    historical_interest_bad = pytrend.get_historical_interest(bad, year_start=beforeYear, month_start=beforeMonth, day_start=beforeDay, hour_start=beforeHour, year_end=currentYear, month_end=currentMonth, day_end=currentDay, hour_end=currentHour, cat=0, geo='', gprop='', sleep=0)
+    historical_interest_bad = historical_interest_bad.drop(labels='isPartial', axis=1) # Delete isPartial
+    historical_interest_bad.insert(0, 'TimeStamp', pd.datetime.now().replace(microsecond=0)) # Insert time
+    
+
+        # GOOD TREND
+    historical_interest_good = pytrend.get_historical_interest(good, year_start=beforeYear, month_start=beforeMonth, day_start=beforeDay, hour_start=beforeHour, year_end=currentYear, month_end=currentMonth, day_end=currentDay, hour_end=currentHour, cat=0, geo='', gprop='', sleep=0)
+    historical_interest_good = historical_interest_good.drop(labels='bitcoin', axis=1) # Delete bitcoin
+    historical_interest_good = historical_interest_good.drop(labels='isPartial', axis=1) # Delete isPartial
+    
+    
+    
+
+        # Query CoinBase Pro ORDER BOOK
+    data = public_client.get_product_order_book('BTC-USD', level=2)
+
+    lBids = data['bids'] # get Bids
+    lBids = list(flatten(lBids)) # One dimension
+    lBids = [float(i) for i in lBids] # Cast str to float
+    del lBids[2::3] # Delete Order Index (Redundancy)
+    dfBids = pd.DataFrame(lBids) # Transform to DataFrame
+    dfBids = dfBids.T # Transpose
+    
+    lAsks = data['asks'] # get Asks
+    lAsks = list(flatten(lAsks)) # One dimension
+    lAsks = [float(i) for i in lAsks] # Cast str to float
+    del lAsks[2::3] # Delete Order Index (Redundancy)
+    dfAsks = pd.DataFrame(lAsks) # Transform to DataFrame
+    dfAsks = dfAsks.T # Transpose
+    
 
 
-l = data['bids']
-l = list(flatten(l))
-l = [float(i) for i in l]
+    # Write all in CSV
+    historical_interest_bad.to_csv(csv_file_name, mode='a', header=False, index=False, line_terminator=',') # Time + Bad Trend
+    historical_interest_good.to_csv(csv_file_name, mode='a', header=False, index=False, line_terminator=',') # Good Trend
+    dfBids.to_csv(csv_file_name, mode='a', header=False, index=False, line_terminator=',') # Order Book Bids
+    dfAsks.to_csv(csv_file_name, mode='a', header=False, index=False) # Order Book Asks
+
+    time.sleep(timeLoop) # 300 sec = 5 min
 
